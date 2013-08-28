27018fb851f1dcb879c3db65927fb2992fa98a55
shenzhun <shenzhunallen@gmail.com>
2013-08-28 16:18:31 +0800

    remove wiki tags to plain text

--- clean_enwiki_xml.sh
+++ clean_enwiki_xml.sh
@@ -1,5 +1,11 @@
 #!/bin/env bash
 
 split -b 200m enwiki-20130805-pages-articles-multistream.xml split-enwiki-part.
-find . -name split-enwiki-part.* > num_tmp
-python xml2files.py
+mv split-enwiki-part.* data/
+cd data/
+rm split-enwiki-part.aa.all.xmltext
+python file2xmltext.py split-enwiki-part.aa.all
+echo "Processing Done!"
+sleep 2
+cat split-enwiki-part.aa.all.xmltext | less
+cd ..
